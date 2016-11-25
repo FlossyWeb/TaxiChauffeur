8dcc5df19e858a51fb40a7d33307939ff2267fcf
FlossyWeb <flossyweb@gmail.com>
2016-11-25 20:15:45 +0100

    Still: WTF dates !

--- js/home.js
+++ js/home.js
@@ -733,6 +733,9 @@ function footer()
 function addCalendar(date, rdv, com, idcourse, cell)
 {
 	/*
+	var startDate = new Date(date.replace(/-/g, '/'));
+	var diff = 60; // difference in minutes
+	var endDate = new Date(startDate.getTime() + diff*60000);
 	var startDate = new Date(2016,11,16,18,30,0,0,0); // beware: month 0 = january, 11 = december 
 	var endDate = new Date(2016,11,16,19,30,0,0,0);
 	var title = "Course en commande";
@@ -742,7 +745,7 @@ function addCalendar(date, rdv, com, idcourse, cell)
 	var success = function(message) { navigator.notification.alert("EVENEMENT AJOUTE AU CALENDRIER", alertDismissed, 'Mon Appli Taxi', 'OK'); };
 	var error = function(message) { navigator.notification.alert("Erreur: " + message, alertDismissed, 'Mon Appli Taxi Erreur', 'OK'); };
 	*/
-	var startDate = new Date(date.replace(/-/g, '/'));
+	var startDate = new Date(date);
 	var diff = 60; // difference in minutes
 	var endDate = new Date(startDate.getTime() + diff*60000);
 	var title = "Course en commande";
